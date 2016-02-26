39e15bc6f04eab25f38abcf83e05232df6993b5e
Chet Kener <Cl3Kener@gmail.com>
2016-02-26 10:12:31 -0500

    Import Brunch/Breakfast Menu
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    * Align brunch & breakfast for pixeldust
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- envsetup.sh
+++ envsetup.sh
@@ -532,6 +532,52 @@ function print_lunch_menu()
     echo
 }
 
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        mka pixeldust
+    else
+        echo "No such item in brunch menu. Try 'breakfast' or 'bib'"
+        return 1
+    fi
+    return $?
+}
+
+function breakfast()
+{
+    target=$1
+    local variant=$2
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/pixeldust/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the PIXELDUST model name
+            if [ -z "$variant" ]; then
+                variant="user"
+            fi
+            lunch pixeldust_$target-$variant
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
 function lunch()
 {
     local answer
